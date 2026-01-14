1cb90bc9d56d8fb1c13e89a0407928bcaa5fd08c
Cheetah555V2 <123528429+Cheetah555V2@users.noreply.github.com>
2026-01-14 23:27:14 +0700

    make graphics and engine compatible with each other

--- graphics.py
+++ graphics.py
@@ -1,25 +1,18 @@
 class GraphicsEngine:
+    def __init__(self) -> None:
+        return None
 
-    def __init__(self, player: object, npcs: list[object]) -> None:
-        self.player = player
-        self.npcs = npcs
-    
-    def render_player_position(self) -> None:
-        print(f"Player is at: {self.player.x} {self.player.y}")
-    
-    def render_npcs_position(self) -> None:
-        for i in range(len(self.npcs)):
-            print(f"NPC {i+1} is at: {self.npcs[i].x} {self.npcs[i].y}")
-    
-    def render(self) -> None:
-        self.render_player_position()
-        self.render_npcs_position()
+    def render(self, player: object, npcs: list[object]) -> None:
+        print(f"Player is at: {player.x} {player.y}")
+        for i in range(len(npcs)):
+            print(f"NPC {i+1} is at: {npcs[i].x} {npcs[i].y}")
+        print()
 
 if __name__ == "__main__":
     class Player:
         def __init__(self, x, y):
-            self.x_pos = x
-            self.y_pos = y
+            self.x = x
+            self.y = y
     
     class NPC(Player):
         def __init__(self, x, y):
@@ -27,10 +20,6 @@ if __name__ == "__main__":
 
     player1 = Player(10, 10)
     npcs = [NPC(20, 20), NPC(30, 30)]
-    gp_engine = GraphicsEngine(player1, npcs)
+    gp_engine = GraphicsEngine()
 
-    gp_engine.render()
-    print()
-    gp_engine.render_npcs_position()
-    print()
-    gp_engine.render_player_position()
+    gp_engine.render(player1, npcs)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from pynput import keyboard
 import time
 import graphics
+import game_engine
 # pip install pynput
 
 
@@ -24,95 +25,35 @@ class KBPoller:
 
         listener = keyboard.Listener(on_press=self.on_press, on_release=self.on_release, suppress=True)
         listener.start()
+    
+    def scan_keys(self):
+        return self.pressed
 
 class NPC:
-    def __init__(self, x_pos, y_pos, x_speed= 1, y_speed= 2):
-        self.x_pos = x_pos
-        self.y_pos = y_pos
-        self.x_speed = x_speed
-        self.y_speed = y_speed
-    
+    def __init__(self, x, y, vx= 1, vy= 2):
+        self.x = x
+        self.y = y
+        self.vx = vx
+        self.vy = vy
+
+class Player(NPC):
+    def __init__(self, x, y, vx= 1, vy= 2):
+        super().__init__(x, y, vx, vy)    
 
 user_keyboard = KBPoller()
 
-running = True
+player1 = Player(20, 20)
 
-npc1 = NPC(10, 10)
-npc2 = NPC(20, 69, 3, 1)
-npc3 = NPC(40, 40, 5, 3)
-
-all_npc = [npc1, npc2, npc3]
-
-player_x = 10
-player_y = 10
-
-x_min = 0
-x_max = 100
-y_min = 0
-y_max = 100
-
-def scan_keys():
-    global user_keyboard
-    return user_keyboard.pressed
+all_npc = [NPC(10, 10), NPC(20, 69, 3, 1), NPC(40, 40, 5, 3)]
 
 
-def render_state():
-    print("player is at:", player_x, player_y)
-    for i in range(len(all_npc)):
-        print(f"npc{i+1} is at: {all_npc[i].x_pos} {all_npc[i].y_pos}")
-    print() #for readability
 
-
-def update_state(inp):
-    global player_x, player_y, running, all_npc
-
-    for i in range(len(all_npc)):
-        all_npc[i].x_pos += all_npc[i].x_speed
-        all_npc[i].y_pos += all_npc[i].y_speed
-
-    if "a" in inp:
-        player_x -= 1
-    if "d" in inp:
-        player_x += 1
-    if "w" in inp:
-        player_y -= 1
-    if "s" in inp:
-        player_y += 1
-    if "q" in inp:
-        running = False
-
-    if player_x < x_min:
-        player_x = x_min
-    if player_x > x_max:
-        player_x = x_max
-    if player_y < y_min:
-        player_y = y_min
-    if player_y > y_max:
-        player_y = y_max
-
-    for i in range(len(all_npc)):
-        if all_npc[i].x_pos >= x_max:
-            all_npc[i].x_pos = x_max
-            all_npc[i].x_speed = -all_npc[i].x_speed
-        elif all_npc[i].x_pos <= x_min:
-            all_npc[i].x_pos = x_min
-            all_npc[i].x_speed = -all_npc[i].x_speed
-        if all_npc[i].y_pos >= y_max:
-            all_npc[i].y_pos = y_max
-            all_npc[i].y_speed = -all_npc[i].y_speed
-        elif all_npc[i].y_pos <= y_min:
-            all_npc[i].y_pos = y_min
-            all_npc[i].y_speed = -all_npc[i].y_speed
-        
-
-while running:
-    # read/check for user actions (input)
-    # update game state (physics, AI, etc)
-    # render game state (graphics)
-
-    render_state()
-    inp = scan_keys()
-
-    update_state(inp)
-
-    time.sleep(1/4)
+if __name__ == "__main__":
+    engine = game_engine.GameEngine(player1,
+                                    all_npc,
+                                    graphics.GraphicsEngine(),
+                                    user_keyboard,
+                                    100,
+                                    100,
+                                    5)
+    engine.run()
