1ecdd0b07555112ba85e85fa71b9757f0d3b0c00
Cheetah555V2 <123528429+Cheetah555V2@users.noreply.github.com>
2026-01-14 11:44:40 +0700

    add npc

--- main.py
+++ main.py
@@ -29,6 +29,12 @@ user_keyboard = KBPoller()
 
 running = True
 
+npc_x = 10
+npc_y = 10
+npc_x_speed = 1
+npc_y_speed = 2
+
+
 player_x = 10
 player_y = 10
 
@@ -44,11 +50,16 @@ def scan_keys():
 
 
 def render_state():
-    print("player is at:", player_x, player_y)
+    #print("player is at:", player_x, player_y)
+    print("npc is at:", npc_x, npc_y)
 
 
 def update_state(inp):
-    global player_x, player_y, running
+    global player_x, player_y, running, npc_x, npc_y, npc_x_speed, npc_y_speed
+
+    npc_x += npc_x_speed
+    npc_y += npc_y_speed
+
     if "a" in inp:
         player_x -= 1
     if "d" in inp:
@@ -69,6 +80,19 @@ def update_state(inp):
     if player_y > y_max:
         player_y = y_max
 
+    if npc_x >= x_max:
+        npc_x = x_max
+        npc_x_speed = -1
+    elif npc_x <= x_min:
+        npc_x = x_min
+        npc_x_speed = 1
+    if npc_y >= y_max:
+        npc_y = y_max
+        npc_y_speed = -2
+    elif npc_y <= y_min:
+        npc_y = y_min
+        npc_y_speed = 2
+    
 
 while running:
     # read/check for user actions (input)
@@ -80,4 +104,4 @@ while running:
 
     update_state(inp)
 
-    time.sleep(1/15)
+    time.sleep(1/4)
