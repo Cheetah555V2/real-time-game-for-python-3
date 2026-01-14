717dcd53876540c4875e63c6ad9e13eede1e16de
Cheetah555V2 <123528429+Cheetah555V2@users.noreply.github.com>
2026-01-14 22:43:35 +0700

    add graphics tester:

--- graphics.py
+++ graphics.py
@@ -14,3 +14,23 @@ class GraphicsEngine:
     def render_all_object_position(self) -> None:
         self.render_player_position()
         self.render_npcs_position()
+
+if __name__ == "__main__":
+    class Player:
+        def __init__(self, x, y):
+            self.x_pos = x
+            self.y_pos = y
+    
+    class NPC(Player):
+        def __init__(self, x, y):
+            super().__init__(x, y)
+
+    player1 = Player(10, 10)
+    npcs = [NPC(20, 20), NPC(30, 30)]
+    gp_engine = GraphicsEngine(player1, npcs)
+
+    gp_engine.render_all_object_position()
+    print()
+    gp_engine.render_npcs_position()
+    print()
+    gp_engine.render_player_position()
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from pynput import keyboard
 import time
+import graphics
 # pip install pynput
 
 
@@ -49,7 +50,6 @@ x_min = 0
 x_max = 100
 y_min = 0
 y_max = 100
-
 
 def scan_keys():
     global user_keyboard
