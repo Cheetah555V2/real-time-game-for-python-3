5e60f3c0f565055eb0259e5998256da19bd33f30
Cheetah555V2 <123528429+Cheetah555V2@users.noreply.github.com>
2026-01-16 11:30:37 +0700

    Change dir for KBPoller to fix the pygame input problem

--- main.py
+++ main.py
@@ -3,38 +3,14 @@ import time
 import graphics
 import game_engine
 from game_object import Player, NPC
-class KBPoller:
-    def on_press(self, key):
-        try:
-            ch = key.char.lower()
-            self.pressed.add(ch)
-        except AttributeError:
-            pass
-
-    def on_release(self, key):
-        try:
-            ch = key.char.lower()
-            self.pressed.remove(ch)
-        except AttributeError:
-            pass
-
-    def __init__(self):
-        self.pressed = set()
-
-        listener = keyboard.Listener(on_press=self.on_press, on_release=self.on_release, suppress=True)
-        listener.start()
-    
-    def scan_keys(self):
-        return self.pressed
-
-user_keyboard = KBPoller()
+from kbpoller import KBPoller
 
 if __name__ == "__main__":
-    engine = game_engine.GameEngine(Player(10, 10),
+    engine = game_engine.PygameGameEngine(Player(10, 10, 10,),
                                     [NPC(20, 20, 1, 2), NPC(30, 30, 2, 3)],
-                                    graphics.GraphicsEngine(),
-                                    user_keyboard,
-                                    100,
-                                    100,
+                                    graphics.PygameGraphicsEngine(),
+                                    "KeyBoard",
+                                    640,
+                                    640,
                                     fps= 5)
     engine.run()
