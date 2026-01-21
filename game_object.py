8d050cc16762aada11ed8d90bddd0732bcceff30
Cheetah555V2 <123528429+Cheetah555V2@users.noreply.github.com>
2026-01-21 11:34:05 +0700

    add angle attribute and update to Player object

--- game_object.py
+++ game_object.py
@@ -1,3 +1,5 @@
+from datatype import Angle
+
 class NPC:
     def __init__(self, x: int, y: int, vx: int = 1, vy: int = 1):
         self.x = x
@@ -24,9 +26,10 @@ class NPC:
         return self.x, self.y
 
 class Player(NPC):
-    def __init__(self, x, y, vx= 1, vy= 1, angle= 0):
+    def __init__(self, x, y, vx= 1, vy= 1, angle= 0, v_angle= 1):
         super().__init__(x, y, vx, vy)
-        self.angle = angle
+        self.angle = Angle(angle)
+        self.v_angle = v_angle
     
     def walk(self, w_pressed: bool, a_pressed: bool, s_pressed: bool, d_pressed: bool):
         if w_pressed:
@@ -37,15 +40,25 @@ class Player(NPC):
             self.y += self.vy
         if d_pressed:
             self.x += self.vx
-    
+
+    def rotate(self, q_pressed: bool, e_pressed: bool) -> None:
+        if q_pressed:
+            self.angle.rotate(self.v_angle)
+        if e_pressed:
+            self.angle.rotate(-self.v_angle)
+
     def update(self, key_pressed: set[str]):
         key_pressed_lower = set()
         for i in key_pressed:
             key_pressed_lower.add(i.lower())
+            
         self.walk('w' in key_pressed_lower,
                   'a' in key_pressed_lower,
                   's' in key_pressed_lower,
                   'd' in key_pressed_lower)
+        
+        self.rotate('q' in key_pressed_lower,
+                    'e' in key_pressed_lower)
     
     def get_position(self):
         return super().get_position()
