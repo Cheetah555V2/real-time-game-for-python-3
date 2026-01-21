3d4c7553db68c9308a76ebb1940d0f539e9853a9
Cheetah555V2 <123528429+Cheetah555V2@users.noreply.github.com>
2026-01-21 11:33:30 +0700

    add Angle datatype:

---
+++ datatype.py
@@ -0,0 +1,53 @@
+import math
+
+class Angle:
+    def __init__(self, degree: float) -> None:
+        self.degree = degree
+    
+    def get_degree(self) -> float:
+        return self.degree
+    
+    def get_radian(self) -> float:
+        return math.pi * 180 / self.degree
+    
+    def rotate(self, angle) -> None:
+        """
+        This function rotate the angle counter-clock wise
+        """
+        self.angle += angle
+        self.angle %= 360
+
+    def cos(self) -> float:
+        return math.cos(self.get_radian())
+
+    def sin(self) -> float:
+        return math.sin(self.get_radian())
+    
+    def tan(self) -> float:
+        return math.tan(self.get_radian())
+
+    def __str__(self) -> str:
+        return str(self.degree)
+    
+    def __repr__(self) -> str:
+        return str(self.degree)
+    
+    def __add__(self, other: object) -> float:
+        try:
+            return (self.degree + other.degree) % 360 # type: ignore
+        except AttributeError:
+            raise AttributeError("Arimatic operation needs to be between Angle object only")
+    
+    def __sub__(self, other: object) -> float:
+        try:
+            return (self.degree - other.degree) % 360 # type: ignore
+        except AttributeError:
+            raise AttributeError("Arimatic operation needs to be between Angle object only")
+    
+
+    
+
+if __name__ == "__main__":
+    x = Angle(180)
+    y = 2
+    print(x + y)
