c8501765838466e35a253fee1a296d6eed1c69b8
Cheetah555V2 <123528429+Cheetah555V2@users.noreply.github.com>
2026-01-26 00:42:57 +0700

    Fix it to be compatible with GameEngine

--- main.py
+++ main.py
@@ -8,16 +8,9 @@ from kbpoller import KBPoller
 if __name__ == "__main__":
     screen_size = (640, 640)
 
-    engine = game_engine.PygameGameEngine(Player(screen_size[0] / 2,# Position X
-                                                 screen_size[1] / 2,# Position Y
-                                                 2,                 # Velocity X
-                                                 2,                 # Velocity Y
-                                                 0,                 # Angle
-                                                 5,                 # Angular Velocity
-                                                 10),               # Radius
-                                    graphics.PygameGraphicsEngine(),
-                                    "KeyBoard",
-                                    screen_size[0],
-                                    screen_size[1],
-                                    fps= 30)
+    engine = game_engine.PygameGameEngine(graphics.PygameGraphicsEngine(),
+                                            "KeyBoard",
+                                            screen_size[0],
+                                            screen_size[1],
+                                            fps= 30)
     engine.run()
