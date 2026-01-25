9770208d2a7f97fe34812dbd0793aa931b2fc9d5
Cheetah555V2 <123528429+Cheetah555V2@users.noreply.github.com>
2026-01-25 20:53:54 +0700

    make the code more readable

--- main.py
+++ main.py
@@ -6,11 +6,19 @@ from game_object import Player, NPC
 from kbpoller import KBPoller
 
 if __name__ == "__main__":
-    engine = game_engine.PygameGameEngine(Player(10, 10, 4, 4),
+    screen_size = (640, 640)
+
+    engine = game_engine.PygameGameEngine(Player(screen_size[0] / 2,# Position X
+                                                 screen_size[1] / 2,# Position Y
+                                                 2,                 # Velocity X
+                                                 2,                 # Velocity Y
+                                                 0,                 # Angle
+                                                 5,                 # Angular Velocity
+                                                 10),               # Radius
                                     [NPC(20, 20, 1, 2), NPC(30, 30, 2, 3)],
                                     graphics.PygameGraphicsEngine(),
                                     "KeyBoard",
-                                    640,
-                                    640,
+                                    screen_size[0],
+                                    screen_size[1],
                                     fps= 10)
     engine.run()
