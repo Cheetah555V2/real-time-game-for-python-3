671107f570f611848bd2dc6eab7983ec2cd1c4b1
Cheetah555V2 <123528429+Cheetah555V2@users.noreply.github.com>
2026-01-26 00:44:53 +0700

    add tutorial level

--- level/tutorial.py
+++ level/tutorial.py
@@ -1,6 +1,13 @@
 import math
-from game_object import Player, NPC, Bullet, Obstacle
+from game_object import Player, NPC, Bullet, Obstacle, Text
 
-player_start_position = ()
-obstacle = [Obstacle(0, 100, 50, 10), Obstacle(200, 150, 10, 50)]
-npcs = [NPC(20, 20, 3, 3), NPC(300, 300, -2, -2)]
+player_start_position = (320, 320)
+obstacles = [Obstacle(0, 300, 640, 10)]
+npcs = [NPC(20, 320, 3, 3), NPC(300, 320, -2, -2)]
+
+texts = [Text("Welcome to the Tutorial Level!", (10, 40), font_size=20, color="blue"),
+         Text("Use W/A/S/D to move the player", (10, 70), font_size=20, color="blue"),
+         Text("Use LEFT/RIGHT arrow keys to rotate the player", (10, 100), font_size=20, color="blue"),
+         Text("Press SPACE to shoot bullets", (10, 130), font_size=20, color="blue"),
+         Text("Shoot all the NPCs until their health are depleated to progress", (10, 160), font_size=20, color="blue"),
+         Text("Good luck!", (10, 190), font_size=20, color="blue")]
